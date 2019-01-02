6ac7efd6e735b71c6df6a50b7df85d7eda31e89a
Jason Goncalves <jgoncalves@protonmail.com>
2019-01-01 22:59:57 -0500

    added footer component and styling

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render () {
+        return (
+            <footer>💡 Created by Jason Goncalves 2019</footer>
+            
+        );
+    }
+}
+
+export default Footer;
