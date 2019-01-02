5913fcf772555b4ee99359560a091a3c71e4660e
Jason Goncalves <jgoncalves@protonmail.com>
2019-01-01 22:54:52 -0500

    added header component and styling

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render () {
+        return (
+            <nav>🔥 Funn Css 🔥</nav>
+            
+        );
+    }
+}
+
+export default Header;
